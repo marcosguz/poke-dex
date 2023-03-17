27d1a0b35115da265f800e4c0c0a9f2470359bd7
marcosguz <marcosguzzz@gmail.com>
2023-03-16 23:53:03 -0500

    PokeDex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { PokemonService } from './services/pokemon.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PokemonListComponent, PokemonDetailComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    SharedModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PokemonService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PokemonList } from '../models/pokemon.list';
+import { PokemonDetail } from '../models/pokemon.detail';
+
+@Injectable({ providedIn: 'root' })
+export class PokemonService {
+  private baseUrl = 'https://pokeapi.co/api/v2/';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(
+    offset: number,
+    limit: number = 20
+  ): Observable<PokemonList[]> {
+    return this.http
+      .get<PokemonList[]>(
+        this.baseUrl + 'pokemon?offset=' + offset + '&limit=' + limit
+      )
+      .pipe(map((x: any) => x.results));
+  }
+
+  getPokemonDetail(pokemon: number | string): Observable<PokemonDetail> {
+    return this.http.get<PokemonDetail>(this.baseUrl + 'pokemon/' + pokemon);
+  }
+}
